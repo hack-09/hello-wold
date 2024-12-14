8d31f0353ad16e89cfffe606e2e9af195e398a4b
Priyanshu Kumar <priyanshukumar9780@gmail.com>
2024-12-14 17:35:37 +0530

    index file update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var url = require('url');
+// var http = require('http');
+// var url = require('url');
 var express = require('express');
 const PORT = 8080;
 
@@ -7,9 +7,16 @@ const app = express();
 
 app.use(express.json());
 
+let books = [
+    { id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { id: 2, title: "1984", author: "George Orwell" }
+];
+
 app.get('/', (req, res)=>{
     res.send("Welcome to my API");
 })
 
-app.listen(PORT,)
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+} )
 
